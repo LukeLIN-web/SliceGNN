cd5d8bf0767cc869092ad611080fb54f9984881e
juyi <1263810658@qq.com>
2022-12-25 17:11:43 +0000

    finished base benchmark

--- base_benchmark.py
+++ base_benchmark.py
@@ -3,8 +3,6 @@ import ast
 
 import torch
 import torch.nn.functional as F
-from tqdm import tqdm
-
 
 import os
 from utils import get_dataset, get_micro_batch
@@ -14,7 +12,7 @@ import quiver
 import torch.distributed as dist
 from torch.nn.parallel import DistributedDataParallel
 import torch.multiprocessing as mp
-from torch.profiler import profile
+import numpy as np
 
 
 class SAGE(torch.nn.Module):
@@ -39,7 +37,7 @@ class SAGE(torch.nn.Module):
         return x.log_softmax(dim=-1)
 
 
-def run(rank, world_size, data, x, quiver_sampler: quiver.pyg.GraphSageSampler,num_classes, args: argparse.ArgumentParser):
+def run(rank, world_size, data, x, quiver_sampler: quiver.pyg.GraphSageSampler, num_classes, args: argparse.ArgumentParser):
     os.environ['MASTER_ADDR'] = 'localhost'
     os.environ['MASTER_PORT'] = '12355'
     dist.init_process_group('nccl', rank=rank, world_size=world_size)
@@ -65,47 +63,40 @@ def run(rank, world_size, data, x, quiver_sampler: quiver.pyg.GraphSageSampler,n
     metrics = {MODEL_SAMPLE: [], MODEL_TRAIN: []}
 
     for epoch in range(1, args.warmup + args.num_epochs + 1):
-        if epoch <= args.warmup:
-            for seeds in train_loader:
-                optimizer.zero_grad()
-                microseeds = seeds[rank * seeds.size(0) // world_size: (
-                    rank + 1) * seeds.size(0) // world_size]
-                n_id, batch_size, adjs = quiver_sampler.sample(microseeds)
-                target_node = n_id[:len(seeds)]
-                micro_batch_adjs = [adj.to(rank)
-                                    for adj in adjs]  # load topo
-                out = model(x[n_id], micro_batch_adjs)  # forward
-                loss = F.nll_loss(out, y[target_node][:batch_size])
-                loss.backward()
-                optimizer.step()
-        else:
-            with torch.profiler.profile(
-                    activities=[
-                        torch.profiler.ProfilerActivity.CPU,
-                        torch.profiler.ProfilerActivity.CUDA,
-                    ],
-                    record_shapes=True,
-                    profile_memory=True,
-                    with_stack=True,
-                    with_flops=True
-                ) as prof:
-                for seeds in train_loader:
-
-                        optimizer.zero_grad()
-                        microseeds = seeds[rank * seeds.size(0) // world_size: (
-                            rank + 1) * seeds.size(0) // world_size]
-                        n_id, batch_size, adjs = quiver_sampler.sample(microseeds)
-                        target_node = n_id[:len(seeds)]
-                        micro_batch_adjs = [adj.to(rank)
-                                            for adj in adjs]  # load topo
-                        out = model(x[n_id], micro_batch_adjs)  # forward
-                        loss = F.nll_loss(out, y[target_node][:batch_size])
-                        loss.backward()
-                        optimizer.step()
-            print(prof.key_averages().table(
-                        sort_by="cpu_time_total", row_limit=10))
-
+        for seeds in train_loader:
+            start = torch.cuda.Event(enable_timing=True)
+            end = torch.cuda.Event(enable_timing=True)
+            start.record()
+            microseeds = seeds[rank * seeds.size(0) // world_size: (
+                rank + 1) * seeds.size(0) // world_size]
+            n_id, batch_size, adjs = quiver_sampler.sample(microseeds)
+            end.record()
+            torch.cuda.synchronize()
+            if epoch >= args.warmup:
+                metrics[MODEL_SAMPLE].append(start.elapsed_time(end))
+            start.record()
+            target_node = n_id[:len(seeds)]
+            micro_batch_adjs = [adj.to(rank)
+                                for adj in adjs]  # load topo
+            out = model(x[n_id], micro_batch_adjs)  # forward
+            loss = F.nll_loss(out, y[target_node][:batch_size])
+            loss.backward()
+            end.record()
+            torch.cuda.synchronize()
+            if epoch >= args.warmup:
+                metrics[MODEL_TRAIN].append(start.elapsed_time(end))
+            optimizer.step()
+            optimizer.zero_grad()
         dist.barrier()
+    if rank == 0:
+        print(f"\nMetrics for GPU {rank}")
+        print(f"Skipped {args.warmup} CUDA warmpup epochs. ")
+        for step, elapsed_milliseconds in metrics.items():
+            A = np.array(elapsed_milliseconds)
+            print(' {N} iterations, {step}, mean={mean} ms, median={median} ms, p90={p90} ms, p99={p99} ms'.format(
+                N=len(A), step=step, mean=np.mean(A),
+                median=np.percentile(A, 50), p90=np.percentile(A, 90),
+                p99=np.percentile(A, 99)))
 
 
 def main(args: argparse.ArgumentParser) -> None:
@@ -125,10 +116,12 @@ def main(args: argparse.ArgumentParser) -> None:
     print('Let\'s use', world_size, 'GPUs!')
     print('BENCHMARK STARTS')
 
+# for batch_size in args.batch_sizes:
+# 就传入, 函数参数多一些没关系.
     mp.spawn(
         run,
         args=(world_size, data, quiver_feature,
-              quiver_sampler,num_classes,  args),
+              quiver_sampler, num_classes,  args),
         nprocs=world_size,
         join=True
     )
