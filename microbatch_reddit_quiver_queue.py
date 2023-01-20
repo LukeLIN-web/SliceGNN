921083e6a95ba7c53f52ee25986193f0d7de1e35
juyi <1263810658@qq.com>
2023-01-20 08:44:40 +0000

    change to Hydra to manage parameters

--- microbatch_reddit_quiver_queue.py
+++ microbatch_reddit_quiver_queue.py
@@ -3,8 +3,10 @@ sample : quiver
 dataset: reddit
 getmicrobatch : yes
 '''
-import argparse
 import os
+
+import hydra
+from omegaconf import OmegaConf
 
 import torch
 import torch.nn.functional as F
@@ -19,78 +21,48 @@ import quiver
 from timeit import default_timer
 from utils.get_micro_batch import get_micro_batch
 from utils.model import SAGE
-from utils.common_config import *
+from utils.common_config import gpu
 
 
-def parse_args(default_run_config):
-    argparser = argparse.ArgumentParser("GCN Training")
+def run_sample(worker_id, params, dataset, quiver_sampler, micro_queues):
+    sample_workers = [gpu(params.num_train_worker + i)
+                      for i in range(params.num_sample_worker)]
 
-    add_common_arguments(argparser, default_run_config)
-
-    argparser.add_argument('--fanout', nargs='+',
-                           type=int, default=default_run_config['fanout'])
-    argparser.add_argument('--lr', type=float,
-                           default=default_run_config['lr'])
-    argparser.add_argument('--dropout', type=float,
-                           default=default_run_config['dropout'])
-    argparser.add_argument('--micro-pergpu', type=int, default=1)
-    return vars(argparser.parse_args())
-
-
-def get_run_config():
-    run_config = {}
-    run_config.update(get_default_common_config(run_mode=RunMode.NORMAL))
-    run_config['sample_type'] = 'khop2'
-    run_config['fanout'] = [25, 10]
-    run_config['lr'] = 0.01
-    run_config['dropout'] = 0.5
-
-    run_config.update(parse_args(run_config))
-
-    process_common_config(run_config)
-    run_config['num_fanout'] = run_config['num_layer'] = len(
-        run_config['fanout'])
-
-    print_run_config(run_config)
-
-    return run_config
-
-
-def run_sample(worker_id, run_config, dataset, quiver_sampler, micro_queues):
-    num_worker = run_config['num_sample_worker']
-    per_gpu = run_config['micro_pergpu']
-    ctx = run_config['sample_workers'][worker_id]
-
+    num_sample_worker = params.num_sample_worker
+    per_gpu = params.micro_pergpu
+    ctx = sample_workers[worker_id]
+    num_train_worker = params.num_train_worker
     print('[Sample Worker {:d}/{:d}] Started with PID {:d}({:s})'.format(
-        worker_id, num_worker, os.getpid(), torch.cuda.get_device_name(ctx)))
+        worker_id, num_sample_worker, os.getpid(), torch.cuda.get_device_name(ctx)))
     data = dataset[0]
     torch.cuda.set_device(ctx)
     train_idx = data.train_mask.nonzero(as_tuple=False).view(-1)
 
     train_loader = torch.utils.data.DataLoader(
-        train_idx, batch_size=1024*run_config['num_train_worker'], shuffle=False, drop_last=True)
+        train_idx, batch_size=1024*num_train_worker, shuffle=False, drop_last=True)
 
     torch.manual_seed(12345)
 
-    for epoch in range(1, run_config['num_epoch']):
+    for epoch in range(1, params.num_epoch+1):
         epoch_start = default_timer()
         for seeds in train_loader:
             n_id, batch_size, adjs = quiver_sampler.sample(seeds)
             micro_batchs = get_micro_batch(adjs,
                                            n_id,
-                                           batch_size, run_config['num_train_worker']*per_gpu)
-            for i in range(run_config['num_train_worker']):
+                                           batch_size, num_train_worker*per_gpu)
+            for i in range(num_train_worker):
                 micro_queues[i].put(
                     (n_id, micro_batchs[i * per_gpu:(i+1) * per_gpu]))
         epoch_end = default_timer()
-        # for i in range(run_config['num_train_worker']):
-            # print(micro_queues[i].qsize()) 
-        print(f'Epoch: {epoch:03d}, Sample Epoch Time: {epoch_end - epoch_start}')
+        print(
+            f'Epoch: {epoch:03d}, Sample Epoch Time: {epoch_end - epoch_start}')
 
 
-def run_train(worker_id, run_config, x,  dataset, queue):
-    ctx = run_config['train_workers'][worker_id]
-    num_worker = run_config['num_train_worker']
+def run_train(worker_id, params, x,  dataset, queue):
+    train_workers = [
+        gpu(i) for i in range(params.num_train_worker)]
+    ctx = train_workers[worker_id]
+    num_worker = params.num_train_worker
 
     if num_worker > 1:
         dist_init_method = 'tcp://{master_ip}:{master_port}'.format(
@@ -107,7 +79,7 @@ def run_train(worker_id, run_config, x,  dataset, queue):
 
     data = dataset[0]
     train_idx = data.train_mask.nonzero(as_tuple=False).view(-1)
-    batch_size = run_config['batch_size']*num_worker
+    batch_size = params.batch_size * num_worker
     length = train_idx.size(dim=0) // (1024*num_worker)
 
     if worker_id == 0:
@@ -116,23 +88,23 @@ def run_train(worker_id, run_config, x,  dataset, queue):
                                           shuffle=False, num_workers=6)
     num_features, num_classes = dataset.num_features, dataset.num_classes
     torch.manual_seed(12345)
-    model = SAGE(num_features, run_config['num_hidden'], num_classes).to(
+    model = SAGE(num_features, params.architecture.hidden_channels, num_classes).to(
         train_device)
     if num_worker > 1:
         model = DistributedDataParallel(
             model, device_ids=[train_device], output_device=train_device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=run_config['lr'])
+    optimizer = torch.optim.Adam(model.parameters(), lr=params.lr)
 
     y = data.y.to(train_device)
 
-    for epoch in range(1, run_config['num_epoch']):
+    for epoch in range(1, params.num_epoch + 1):
         model.train()
         epoch_start = default_timer()
         for i in range(length):
             optimizer.zero_grad()
             (n_id, micro_batchs) = queue.get()
             target_node = n_id[:batch_size][worker_id *
-                                            run_config['batch_size']: (worker_id+1)*run_config['batch_size']]
+                                            params.batch_size: (worker_id+1)*params.batch_size]
             for i in range(len(micro_batchs)):
                 micro_batch = micro_batchs[i]
                 micro_batch_adjs = [adj.to(train_device)
@@ -144,7 +116,6 @@ def run_train(worker_id, run_config, x,  dataset, queue):
                 loss.backward()
             optimizer.step()
         epoch_end = default_timer()
-        # print(queue.qsize())
 
         if worker_id == 0:
             print(
@@ -164,10 +135,14 @@ def run_train(worker_id, run_config, x,  dataset, queue):
             print(f'Train: {acc1:.4f}, Val: {acc2:.4f}, Test: {acc3:.4f}')
 
 
-if __name__ == '__main__':
-    run_config = get_run_config()
-    num_train_workers = run_config['num_train_worker']
-    num_sample_worker = run_config['num_sample_worker']
+@hydra.main(config_path='conf', config_name='config', version_base='1.1')
+def main(conf):
+    model_name, dataset_name = conf.model.name, conf.dataset.name
+    params = conf.model.params[dataset_name]
+
+    num_train_workers = params.num_train_worker
+    num_sample_worker = params.num_sample_worker
+    print(OmegaConf.to_yaml(conf))
 
     dataset = Reddit('/data/Reddit')
     data = dataset[0]
@@ -185,15 +160,19 @@ if __name__ == '__main__':
     microbatchs_qs = [mp.Queue(30) for i in range(num_train_workers)]
     for worker_id in range(num_sample_worker):
         p = mp.Process(target=run_sample, args=(
-            worker_id, run_config, dataset, quiver_sampler, microbatchs_qs))
+            worker_id, params, dataset, quiver_sampler, microbatchs_qs))
         p.start()
         workers.append(p)
 
     for worker_id in range(num_train_workers):
         p = mp.Process(target=run_train, args=(
-            worker_id, run_config, quiver_feature, dataset, microbatchs_qs[worker_id]))
+            worker_id, params, quiver_feature, dataset, microbatchs_qs[worker_id]))
         p.start()
         workers.append(p)
 
     for p in workers:
         p.join()
+
+
+if __name__ == '__main__':
+    main()
