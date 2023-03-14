3551ec9231ebc20eebfd403fc9c5c558647e0deb
juyi <1263810658@qq.com>
2023-03-14 17:43:16 +0000

    refactor

--- microGNN/prune.py
+++ microGNN/prune.py
@@ -5,27 +5,29 @@ from microGNN.utils import slice_adj
 from microGNN.utils.common_class import Adj
 
 
-def prune(
-    target_node: Tensor, adjs: list, cached_nodes: Tensor
-) -> Tuple[Tensor, Tensor]:
+def prune(target_node: Tensor, adjs: list, cached_nodes: Tensor) -> Tuple[Tensor, list]:
+    r"""
+    Prune the nano batch graph, only keep the nodes that are not in the cached_nodes.
+
+    Args:
+        target_node (Tensor): The target node.
+        adjs (list): A list of adjacency matrices.
+        cached_nodes (Tensor): The cached nodes.
+
+    :rtype: (all node, list)
+    """
     adjs.reverse()
     new_adjs = []
     layernode = target_node
     for i, adj in enumerate(adjs):
-        adj = adjs[i]
-        print(adj)
-        print(layernode)
         mask = ~torch.isin(layernode, cached_nodes[i])
         sub_nid = layernode[mask]
         # target node in layer i 或者可以unique一下, 对比一下unique快还是不unique快
-        # print(mask)  # finnd the node in layer i that is in cached_nodes
-        print(sub_nid)
+        # find the node in layer i that is in cached_nodes
         target_size = len(sub_nid)
         layernode, sub_adjs, edge_mask = slice_adj(
             sub_nid, adj.edge_index, relabel_nodes=False
         )
-        # print(layernode)
-        # print(sub_adjs)
         new_adjs.append(Adj(sub_adjs, None, (len(layernode), target_size)))
     new_adjs.reverse()
     return layernode, adjs
--- test/test_prune.py
+++ test/test_prune.py
@@ -14,11 +14,6 @@ x = torch.tensor(
 
 
 def test_getprune():
-    """
-    1. 生成一个batch
-    2. 生成两个nano batch
-    3. prune确认正确性
-    """
     hop = [1, 3]
     train_loader = NeighborSampler(
         edge_index,
@@ -35,23 +30,18 @@ def test_getprune():
     print(n_id.dtype)
 
 
+# 验证prune正确性
 def test_prune():
-    """
-    验证prune正确性
-    """
     edge1 = torch.tensor([[3, 4], [2, 2]])
     adjs1 = Adj(edge1, None, (3, 1))
     edge2 = torch.tensor([[3, 4, 6, 7, 5, 8], [2, 2, 3, 3, 4, 4]])
     adjs2 = Adj(edge2, None, (7, 3))
-    adjs = [adjs2, adjs1]
-    n_id = torch.tensor([2, 3, 4, 5, 6, 7, 8])  # 第二hop会排序吗? 不一定. 不过hop1都在hop2前面
-    batch_size = 1
+    adjs = [adjs2, adjs1]  # hop2会排序吗? 不一定. 不过hop1节点都在hop2前面
     target_node = torch.tensor([2])
-    # print(n_id, adjs)
-    sel_nid = torch.tensor([[40, 3, 20], [23, 4, 30]])  # target node, 1hop, 2hop ...
-    # print(sel_nid)
-    sub_n_id, sub_adjs = prune(target_node, adjs, sel_nid)
-    print(sub_adjs)
+    cached_nodes = torch.tensor(
+        [[40, 3, 20], [23, 4, 30]]
+    )  # target node, 1hop, 2hop ...
+    sub_n_id, sub_adjs = prune(target_node, adjs, cached_nodes)
     assert torch.all(torch.eq(sub_n_id, torch.tensor([2, 3, 4, 6, 7])))
     assert torch.all(
         torch.eq(
