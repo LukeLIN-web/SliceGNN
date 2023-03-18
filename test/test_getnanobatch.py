2c0693e3de1166f7d9fab78c8192417efb868eb9
juyi <1263810658@qq.com>
2023-03-18 10:46:03 +0000

    refactor get nano batch

--- test/test_getnanobatch.py
+++ test/test_getnanobatch.py
@@ -1,61 +1,41 @@
-from torch import Tensor
-from typing import List
-from torch_geometric.loader import NeighborSampler
 import torch
-from microGNN.utils.get_nano_batch import *
-import torch.nn.functional as F
-from torch_geometric.nn import SAGEConv
+from torch import Tensor
+from torch_geometric.loader import NeighborSampler
 
-edge_index = torch.tensor([[0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 9, 9, 9],
-                           [1, 6, 0, 2, 6, 1, 3, 7, 2, 4, 8, 3, 5, 9, 4, 9, 0, 1, 7, 2, 6, 8, 3, 7, 9, 4, 5, 8]], dtype=torch.long)  # fmt: skip
-x = Tensor(
-    [[1, 2], [2, 3], [3, 3], [4, 3], [5, 3], [6, 3], [7, 3], [8, 3], [9, 3], [10, 3]]
-)
+from microGNN.models import SAGE
+from microGNN.utils import get_nano_batch, slice_adj
+
+# yapf: disable
+edge_index = torch.tensor([
+    [0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 9, 9, 9], # noqa
+    [1, 6, 0, 2, 6, 1, 3, 7, 2, 4, 8, 3, 5, 9, 4, 9,0, 1, 7, 2, 6, 8, 3, 7, 9, 4, 5, 8 ]],dtype=torch.long)  # noqa
+x = Tensor([[1, 2], [2, 3], [3, 3], [4, 3], [5, 3], [6, 3], [7, 3], [8, 3],
+            [9, 3], [10, 3]])
+# yapf: enable
 num_features, hidden_size, num_classes = 2, 16, 1
 
 
-class SAGE(torch.nn.Module):
-    def __init__(self, in_channels, hidden_channels, out_channels, num_layers=2):
-        super().__init__()
-        self.num_layers = num_layers
-
-        self.convs = torch.nn.ModuleList()
-        self.convs.append(SAGEConv(in_channels, hidden_channels))
-        for _ in range(self.num_layers - 2):
-            self.convs.append(SAGEConv(hidden_channels, hidden_channels))
-        self.convs.append(SAGEConv(hidden_channels, out_channels))
-
-    def forward(self, x, adjs):
-        assert len(adjs[0]) == 3
-        for i, (edge_index, _, size) in enumerate(adjs):
-            x_target = x[: size[1]]  # Target nodes are always placed first.
-            x = self.convs[i]((x, x_target), edge_index)
-            if i != self.num_layers - 1:
-                x = F.relu(x)
-        return x
-
-
 def test_slice_adj():
-    edge_index = torch.tensor(
-        [
-            [0, 1, 2, 3, 4, 5],
-            [2, 2, 4, 4, 6, 6],
-        ]
-    )
+    edge_index = torch.tensor([
+        [0, 1, 2, 3, 4, 5],
+        [2, 2, 4, 4, 6, 6],
+    ])
 
-    subset, edge_index, edge_mask = slice_adj(6, edge_index, relabel_nodes=True)
+    subset, edge_index, edge_mask = slice_adj(6,
+                                              edge_index,
+                                              relabel_nodes=True)
     assert subset.tolist() == [6, 4, 5]
     assert edge_index.tolist() == [[1, 2], [0, 0]]
     assert edge_mask.tolist() == [False, False, False, False, True, True]
 
-    edge_index = torch.tensor(
-        [
-            [1, 2, 4, 5],
-            [0, 1, 5, 6],
-        ]
-    )
+    edge_index = torch.tensor([
+        [1, 2, 4, 5],
+        [0, 1, 5, 6],
+    ])
 
-    subset, edge_index, edge_mask = slice_adj([0, 6], edge_index, relabel_nodes=True)
+    subset, edge_index, edge_mask = slice_adj([0, 6],
+                                              edge_index,
+                                              relabel_nodes=True)
 
     assert subset.tolist() == [0, 6, 1, 5]
     assert edge_index.tolist() == [[2, 3], [0, 1]]
@@ -74,6 +54,7 @@ def test_get_nano_batch():
         drop_last=True,
     )
     model = SAGE(num_features, hidden_size, num_classes, num_layers=3)
+    model.eval()
     for batch_size, n_id, adjs in train_loader:
         out = model(x[n_id], adjs)
         num_micro_batch = 4
@@ -95,6 +76,7 @@ def test_get_nano_batch():
         drop_last=True,
     )
     model = SAGE(num_features, hidden_size, num_classes)
+    model.eval()
     for batch_size, n_id, adjs in train_loader:
         out = model(x[n_id], adjs)
         num_micro_batch = 4
