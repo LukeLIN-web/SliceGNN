fix bug : pull don't pass gradient.  remove the @torch.no_grad() in pull function.

microGNN/history.py
@@ -23,8 +23,7 @@ class History(torch.nn.Module):
                                        dtype=torch.bool,
                                        device=device,
                                        pin_memory=pin_memory)
-
-        self._device = torch.device("cpu")
+        self.device = device  # memory device
 
         self.reset_parameters()
 
@@ -32,21 +31,16 @@ class History(torch.nn.Module):
         self.emb.fill_(0)
         self.cached_nodes.fill_(False)
 
-    def _apply(self, fn):
-        # Set the `_device` of the module without transfering `self.emb`.
-        self._device = fn(torch.zeros(1)).device
-        return self
-
-    @torch.no_grad()
     def pull(self, x: Tensor, n_id: Tensor) -> Tensor:
         cached_nodes = self.cached_nodes[
             n_id]  # get cached_nodes for the given node ids
         emb = self.emb[n_id]  # get embeddings for the cached nodes
         mask = cached_nodes.unsqueeze(1).expand(
             x.size(0), x.size(1))  # expand to the same shape as x
-        x.masked_fill_(mask, 0)  # set the values of cached nodes in x to 0
-        x += emb  # add the embeddings of the cached nodes to x
-        return x
+        out = x.clone()
+        out.masked_fill_(mask, 0)  # set the values of cached nodes in x to 0
+        out += emb  # add the embeddings of the cached nodes to x
+        return out
 
     @torch.no_grad()
     def push(

test/test_history.py
@@ -136,7 +136,6 @@ def test_gradient(device):
     loss1 = F.nll_loss(out1, y[target_node][nb.size])
     loss1.backward()
     for param in model1.parameters():
-        print(param)
         print(param.grad)
         assert param.grad is not None
     grad1 = [
@@ -291,5 +290,6 @@ def test_pull_and_push():
 
 if __name__ == "__main__":
     # test_pull_and_push()
-    test_history_function()
+    # test_history_function()
+    test_gradient("cpu")
     # test_same_out("cpu")