ab53c8d65258e01e1f95cc6131cfeaaa1fd7cb4d
juyi <1263810658@qq.com>
2023-04-17 12:35:16 +0000

    speed up, from 77s to 14s in ogbnproduct

--- microGNN/history.py
+++ microGNN/history.py
@@ -48,31 +48,47 @@ class History(torch.nn.Module):
         self.push(x, inter_id)
 
     def pull(self, x: Tensor, inter_id: Tensor, layer_id: Tensor) -> Tensor:
+        # out = x.clone()
+        # for id in inter_id:
+        #     if self.cached_nodes[id]:
+        #         # print("pulling")
+        #         embidx = torch.where(self.global_idx == id)[0]
+        #         emb = self.emb[embidx]
+        #         xidx = torch.where(layer_id == id)[0]
+        #         out[xidx] = emb
+        #     else:
+        #         # print("need cache , but not pushed")
+        #         pass
+        # return out
         out = x.clone()
-        for id in inter_id:
-            if self.cached_nodes[id]:
-                # print("pulling")
-                embidx = torch.where(self.global_idx == id)[0]
-                emb = self.emb[embidx]
-                xidx = torch.where(layer_id == id)[0]
-                out[xidx] = emb
-            else:
-                # print("need cache , but not pushed")
-                pass
+        cached_id = inter_id[self.cached_nodes[inter_id]]
+        cached_embidx = torch.where(
+            self.global_idx.unsqueeze(-1) == cached_id)[0]
+        if len(cached_embidx) > 0:
+            cached_xidx = torch.where(layer_id.unsqueeze(-1) == cached_id)[0]
+            out[cached_xidx, :] = self.emb[cached_embidx[:len(cached_xidx)], :]
         return out
 
     @torch.no_grad()
     def push(self, x: Tensor, inter_id: Tensor, layer_id: Tensor) -> Tensor:
-        for id in inter_id:
-            if self.cached_nodes[id]:
-                # print("have pushed")
-                pass
-            else:
-                # print("pushing")
-                embidx = torch.where(self.global_idx == id)[0]
-                xidx = torch.where(layer_id == id)[0]
-                self.emb[embidx] = x[xidx]
-                self.cached_nodes[id] = True
+        # for id in inter_id:
+        #     if self.cached_nodes[id]:
+        #         # print("have pushed")
+        #         pass
+        #     else:
+        #         # print("pushing")
+        #         embidx = torch.where(self.global_idx == id)[0]
+        #         xidx = torch.where(layer_id == id)[0]
+        #         self.emb[embidx] = x[xidx]
+        #         self.cached_nodes[id] = True
+        uncached_id = inter_id[~self.cached_nodes[inter_id]]
+        uncached_embidx = torch.where(
+            self.global_idx.unsqueeze(-1) == uncached_id)[0]
+        if len(uncached_embidx) > 0:
+            uncached_xidx = torch.where(
+                layer_id.unsqueeze(-1) == uncached_id)[0]
+            self.emb[uncached_embidx] = x[uncached_xidx]
+            self.cached_nodes[uncached_id] = True
 
     def forward(self, *args, **kwargs):
         """"""
